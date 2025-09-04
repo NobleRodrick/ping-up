1d678272598abc8803b1a5baecf20fec2ad03bbb
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2025-09-04 18:20:59 +0100

    some works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -67,7 +67,7 @@ const App = () => {
     <>
       <Toaster />
         <Routes>
-          <Route path="/" element={user ? <Layout /> : <Login />}> 
+          <Route path="/" element={!user ? <Login /> : <Layout />}> 
             <Route index element={<Feed />} />
             <Route path="messages" element={<Messages />} />
             <Route path="messages/:userId" element={<ChatBox />} />
